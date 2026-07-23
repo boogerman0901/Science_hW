8528ff450217230e862de334631012576b25a5f9
boogerman0901 <brodieeeeez@outlook.com>
2026-07-23 10:30:55 +1000

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare: "https://duckdns.org",
+    bare: "https://raw.githubusercontent.com/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
